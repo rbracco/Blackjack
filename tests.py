96706a994f97345c764a34e3a6174cff6954a51f
Zach Tibbitts <zachtib@gmail.com>
2020-05-13 13:59:47 -0400

    add tests

--- tests.py
+++ tests.py
@@ -1,6 +1,8 @@
 import unittest
 
 from card import Card
+from deck import Deck
+from hand import Hand
 
 
 class CardTestCase(unittest.TestCase):
@@ -11,6 +13,40 @@ class CardTestCase(unittest.TestCase):
     def test_is_facecard_for_king(self):
         card = Card(13, Card.HEARTS)
         self.assertTrue(card.is_facecard())
+
+
+class HandTestCase(unittest.TestCase):
+    def test_three_kings_is_bust(self):
+        hand = Hand()
+        hand.add_card(Card(13, Card.SPADES))
+        hand.add_card(Card(13, Card.HEARTS))
+        hand.add_card(Card(13, Card.CLUBS))
+
+        self.assertTrue(hand.is_bust())
+
+
+class DeckTestCase(unittest.TestCase):
+    def test_initial_deck_has_52_cards(self):
+        deck = Deck()
+
+        self.assertEqual(len(deck.cards), 52)
+
+    def test_draw_returns_top_card(self):
+        deck = Deck()
+        top_card = deck.cards[0]
+
+        drawn_card = deck.draw()
+
+        self.assertEqual(top_card.rank, drawn_card.rank)
+        self.assertEqual(top_card.suit, drawn_card.suit)
+
+    def test_drawing_reduces_deck_size_by_1(self):
+        deck = Deck()
+        initial_size = len(deck.cards)
+        deck.draw()
+        size = len(deck.cards)
+
+        self.assertEqual(size, initial_size - 1)
 
 
 if __name__ == '__main__':
